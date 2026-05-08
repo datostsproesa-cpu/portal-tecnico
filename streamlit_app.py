901fcdbe0a340e2afc9cb2e87f8a7ee960ef4c73
datostsproesa-cpu <datos.ts.proesa@gmail.com>
2026-05-08 10:21:13 -0500

    streamlit_app.py
    KUY

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Portal de Servicio Técnico", layout="centered")
 
 # --- CONFIGURACIÓN DE ENLACES EXTERNOS ---
 # ✅ AQUÍ AGREGAS TU NUEVO ENLACE TÉCNICO
-URL_FORMULARIO_TECNICO = "AQUÍ_PEGA_LA_NUEVA_URL_QUE_TE_DIO_APPS_SCRIPT"
+URL_FORMULARIO_TECNICO = "https://script.google.com/macros/s/AKfycbyNf5S6Wf2p7xX5W1YhvTOaVMpg-iOt3l0wAYLxYCDCZ3HcCueVzT_rYr9O13ONU637aQ/exec"
 
 # --- INTERFAZ ---
 st.markdown("<h2 style='text-align: center;'>Módulo Técnico</h2>", unsafe_allow_html=True)
