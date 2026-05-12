3f14aa0dc7ef07fc8586e481134bc9e7ae55a08b
datostsproesa-cpu <datos.ts.proesa@gmail.com>
2026-05-12 17:39:08 -0500

    streamlit_app.py
    v3

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ st.divider()
 st.info("Selecciona el módulo al que deseas acceder. Los datos se guardarán automáticamente en tu base de datos.")
 
 # --- ENLACES AL APPS SCRIPT (Asegúrate de poner la URL de tu implementación final) ---
-URL_BASE_SCRIPT = "https://script.google.com/macros/s/AKfycbzkI5LVU8JMWdrHwgsDgjCwVpNhp0YqSn1XnC5TPUcKUc_HKAOmR3BbgWNRWfkS7If_/exec"
+URL_BASE_SCRIPT = "https://script.google.com/macros/s/AKfycbwC8p7qkNlJvQFXIGHdjjllWm19mhIbFKCbExj7My4S0hY8Tu6JAtSKa9ODW55QqlpA/exec"
 
 URL_CONTACTOS = f"{URL_BASE_SCRIPT}?page=contactos"
 URL_VISITAS = f"{URL_BASE_SCRIPT}?page=visitas"
